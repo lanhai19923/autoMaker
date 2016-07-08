6fadd9d99a250ad675473dfb988326f814f21b1a
chenyaoyi <chenyaoyi@ppmoney.com>
2016-07-08 15:42:27 +0800

    eventHandler.js增加rely方法

--- eventHandler.js
+++ eventHandler.js
@@ -1,5 +1,7 @@
-﻿var eventHandler = function () {
+﻿var eventHandler = function (host) {
 	this.eventList = {};
+	this.eventRely = {};
+	this.host = host;
 };
 eventHandler.prototype.on = function (eventName, handler) {
 	if (typeof eventName != 'string') {
@@ -14,6 +16,17 @@ eventHandler.prototype.on = function (eventName, handler) {
 		this.eventList[eventName] = [];
 	}
 	this.eventList[eventName].push(handler);
+	return this;//to support chain method
+};
+eventHandler.prototype.rely = function (eventName, relyEventList) {
+	if (this.eventRely[eventName] == undefined) {
+		this.eventRely[eventName] = [];
+	} 
+	for (var i = 0; i < relyEventList.length; i++) {
+		if (this.eventRely[eventName].indexOf(relyEventList[i]) == -1) {
+			this.eventRely[eventName].push(relyEventList[i]);
+		}
+	}
 };
 eventHandler.prototype.off = function (eventName, handler) {
 	if (typeof eventName != 'string') {
@@ -23,6 +36,7 @@ eventHandler.prototype.off = function (eventName, handler) {
 	if (handler == undefined) {
 		//remove all handlers
 		this.eventList[eventName] = [];
+		return this;//to support chain method
 	} else {
 		if (typeof handler != 'function') {
 			console.log("event handler must be a function!");
@@ -45,9 +59,35 @@ eventHandler.prototype.emit = function (eventName) {
 		console.log("eventName must be a string!");
 		return false;
 	}
+	if (!this.eventList[eventName]) {
+		console.log(eventName + " is not defined");
+		return;
+	}
 	for (var i = 0; i < this.eventList[eventName].length; i++) {
 		var handler = this.eventList[eventName][i];
-		handler();
+		if (!!this.host) {
+			handler.call(this.host);
+		} else {
+			handler();
+		}
+	}
+	this.checkRely(eventName);
+};
+eventHandler.prototype.checkRely = function (eventName) {
+	for (var relyEvent in this.eventRely) {
+		for (var i = 0; i < this.eventRely[relyEvent].length; i++) {
+			if (this.eventRely[relyEvent][i] == eventName) {
+				this.eventRely[relyEvent].splice(i,1);
+				break;
+			}
+		}
+	}
+	for (var relyEvent in this.eventRely) {
+		if (this.eventRely[relyEvent].length == 0) {
+			var relyEventName = relyEvent;
+			delete this.eventRely[relyEvent];
+			this.emit(relyEventName);
+		}
 	}
 };
 exports.eventHandler = eventHandler;
